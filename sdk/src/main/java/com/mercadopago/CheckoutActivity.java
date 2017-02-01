37078d0bc733bbb6d51c68773ade24f370e2c2a8
valeserber <valserber@gmail.com>
2017-02-01 16:57:27 -0300

    trying to start payment vault activity

--- sdk/src/main/java/com/mercadopago/CheckoutActivity.java
+++ sdk/src/main/java/com/mercadopago/CheckoutActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.mercadopago.core.MercadoPago;
+import com.mercadopago.core.MercadoPagoComponents;
 import com.mercadopago.core.MercadoPagoContext;
 import com.mercadopago.model.Site;
 import com.mercadopago.preferences.CheckoutPreference;
@@ -14,6 +16,8 @@ import com.mercadopago.preferences.ServicePreference;
 import com.mercadopago.presenters.CheckoutPresenter;
 import com.mercadopago.util.JsonUtil;
 import com.mercadopago.views.CheckoutActivityView;
+
+import java.math.BigDecimal;
 
 /**
  * Created by vaserber on 2/1/17.
@@ -26,6 +30,7 @@ public class CheckoutActivity extends AppCompatActivity implements CheckoutActiv
 
     protected FlowPreference mFlowPreference;
     protected Site mSite;
+    protected String mPublicKey;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,16 +41,16 @@ public class CheckoutActivity extends AppCompatActivity implements CheckoutActiv
         mPresenter.setView(this);
         mActivity = this;
         setContentView();
-        if (savedInstanceState == null) {
+//        if (savedInstanceState == null) {
             getActivityParameters();
             mPresenter.validateActivityParameters();
-        }
+//        }
     }
 
-    private void initializeMercadoPagoContext(String publicKey, DecorationPreference decorationPreference,
+    private void initializeMercadoPagoContext(DecorationPreference decorationPreference,
                                               ServicePreference servicePreference, CheckoutPreference checkoutPreference) {
         new MercadoPagoContext.Builder()
-                .setPublicKey(publicKey)
+                .setPublicKey(mPublicKey)
                 .setDecorationPreference(decorationPreference)
                 .setServicePreference(servicePreference)
                 .setCheckoutPreference(checkoutPreference)
@@ -53,13 +58,13 @@ public class CheckoutActivity extends AppCompatActivity implements CheckoutActiv
     }
 
     private void getActivityParameters() {
-        String publicKey = getIntent().getStringExtra("publicKey");
+        mPublicKey = getIntent().getStringExtra("publicKey");
         CheckoutPreference checkoutPreference = JsonUtil.getInstance().fromJson(getIntent().getStringExtra("checkoutPreference"), CheckoutPreference.class);
         DecorationPreference decorationPreference = JsonUtil.getInstance().fromJson(getIntent().getStringExtra("decorationPreference"), DecorationPreference.class);
         ServicePreference servicePreference = JsonUtil.getInstance().fromJson(getIntent().getStringExtra("servicePreference"), ServicePreference.class);
         mFlowPreference = JsonUtil.getInstance().fromJson(getIntent().getStringExtra("flowPreference"), FlowPreference.class);
 
-        initializeMercadoPagoContext(publicKey, decorationPreference, servicePreference, checkoutPreference);
+        initializeMercadoPagoContext(decorationPreference, servicePreference, checkoutPreference);
     }
 
 
@@ -70,7 +75,7 @@ public class CheckoutActivity extends AppCompatActivity implements CheckoutActiv
                 && !checkoutPreference.getItems().isEmpty()) {
             mSite = new Site(checkoutPreference.getSiteId(), checkoutPreference.getItems().get(0).getCurrencyId());
         }
-
+        startPaymentVaultActivity();
     }
 
     @Override
@@ -80,5 +85,28 @@ public class CheckoutActivity extends AppCompatActivity implements CheckoutActiv
 
     private void setContentView() {
         setContentView(R.layout.mpsdk_activity_checkout);
+    }
+
+    protected void startPaymentVaultActivity() {
+        BigDecimal amount = MercadoPagoContext.getInstance().getCheckoutPreference().getAmount();
+        new MercadoPagoComponents.Activities.PaymentVaultActivityBuilder()
+                .setActivity(this)
+                .setSite(mSite)
+                .setAmount(amount)
+                .setMerchantPublicKey(mPublicKey)
+//                .setPaymentMethodSearch(mPaymentMethodSearch)
+                .startActivity();
+
+
+//        new MercadoPago.StartActivityBuilder()
+//                .setActivity(this)
+//                .setPublicKey(mMerchantPublicKey)
+//                .setSite(mSite)
+//                .setAmount(mCheckoutPreference.getAmount())
+//                .setPaymentMethodSearch(mPaymentMethodSearch)
+//                .setPaymentPreference(mCheckoutPreference.getPaymentPreference())
+//                .setDecorationPreference(mDecorationPreference)
+//                .setCards(mSavedCards)
+//                .startPaymentVaultActivity();
     }
 }
--- sdk/src/main/java/com/mercadopago/presenters/CheckoutPresenter.java
+++ sdk/src/main/java/com/mercadopago/presenters/CheckoutPresenter.java
@@ -4,10 +4,16 @@ import android.content.Context;
 
 import com.mercadopago.callbacks.Callback;
 import com.mercadopago.callbacks.FailureRecovery;
+import com.mercadopago.core.MercadoPago;
+import com.mercadopago.core.MercadoPagoContext;
+import com.mercadopago.core.MercadoPagoServices;
 import com.mercadopago.model.ApiException;
 import com.mercadopago.model.PaymentMethodSearch;
+import com.mercadopago.preferences.CheckoutPreference;
 import com.mercadopago.util.ApiUtil;
 import com.mercadopago.views.CheckoutActivityView;
+
+import java.math.BigDecimal;
 
 /**
  * Created by vaserber on 2/1/17.
@@ -16,7 +22,9 @@ import com.mercadopago.views.CheckoutActivityView;
 public class CheckoutPresenter {
 
     private Context mContext;
+    private String mPublicKey;
     private CheckoutActivityView mView;
+    private MercadoPagoServices mMercadoPagoServices;
 
     public CheckoutPresenter(Context context) {
         this.mContext = context;
@@ -35,5 +43,45 @@ public class CheckoutPresenter {
             mView.onInvalidStart("error");
         }
     }
+
+    private void initializeMercadoPagoServices() {
+        if (mPublicKey == null || mMercadoPagoServices != null) return;
+        mMercadoPagoServices = new MercadoPagoServices.Builder()
+                .setContext(mContext)
+                .setPublicKey(mPublicKey)
+                .build();
+    }
+
+//    public void getPaymentMethodSearch() {
+//        initializeMercadoPagoServices();
+//
+//        //BigDecimal amount, List<String> excludedPaymentTypes, List<String> excludedPaymentMethods, Payer payer, boolean accountMoneyEnabled, final Callback<PaymentMethodSearch> callback
+//        CheckoutPreference checkoutPreference = MercadoPagoContext.getInstance().getCheckoutPreference();
+//
+//        mMercadoPagoServices.getPaymentMethodSearch(checkoutPreference.getAmount(), checkoutPreference.getExcludedPaymentTypes(), checkoutPreference.getExcludedPaymentMethods(), checkoutPreference.getPayer(), false, new Callback<PaymentMethodSearch>() {
+//            @Override
+//            public void success(PaymentMethodSearch paymentMethodSearch) {
+//                mPaymentMethodSearch = paymentMethodSearch;
+//                if (!mPaymentMethodSearch.hasSavedCards() && isMerchantServerInfoAvailable()) {
+//                    getCustomerAsync();
+//                } else if (isActivityActive()) {
+//                    startPaymentVaultActivity();
+//                }
+//            }
+//
+//            @Override
+//            public void failure(ApiException apiException) {
+//                if (isActivityActive()) {
+//                    setFailureRecovery(new FailureRecovery() {
+//                        @Override
+//                        public void recover() {
+//                            getPaymentMethodSearch();
+//                        }
+//                    });
+//                    ApiUtil.showApiExceptionError(getActivity(), apiException);
+//                }
+//            }
+//        });
+//    }
 
 }
